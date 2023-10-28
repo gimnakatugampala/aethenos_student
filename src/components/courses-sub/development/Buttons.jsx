5a503e23b08a19f39dd00d621f9141eca0a6bc75
gimnakatugampala <gimnakatugampala1@gmail.com>
2023-10-28 11:15:49 +0530

    2023_10_28_topics

--- src/components/courses-sub/development/Buttons.jsx
+++ src/components/courses-sub/development/Buttons.jsx
@@ -1,28 +1,49 @@
 import React, { useState } from "react";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
+import CardContainer from "../../../contexts/CardContainer";
 
 const ButtonCarousel = () => {
   const items = [
     [
-      { title: "Web Development", link: "development/web_development" },
-      { title: "Data Science", link: "development/data_science" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
+      { title: "Web Development", link: "web-development-link-here" },
+      { title: "Software Development", link: "reactjs-link-here" },
+      { title: "React JS", link: "css-link-here" },
+      { title: "Redux Framework", link: "redux-framework-link-here" },
+      { title: "Data Scientist", link: "web-development-link-here" },
+      { title: "Artificial Intelligence", link: "reactjs-link-here" },
+      { title: "HTML & CSS", link: "css-link-here" },
+      { title: "Accounting", link: "redux-framework-link-here" },
     ],
     [
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
+      { title: "Stock Trading", link: "typescript-link-here" },
+      { title: "Financial Analysis", link: "javascript-link-here" },
+      { title: "Cryptocurrency", link: "angular-link-here" },
+      { title: "Forex Trading", link: "nodejs-link-here" },
+      { title: "Financial Market", link: "typescript-link-here" },
+      { title: "Alogorithm Anaylisys", link: "javascript-link-here" },
+      { title: "Investing", link: "angular-link-here" },
+      { title: "Excel", link: "nodejs-link-here" },
+    ],
+    [
+      { title: "Day Trading", link: "web-development-link-here" },
+      { title: "Quick Books Online", link: "reactjs-link-here" },
+      { title: "Book Keeping", link: "css-link-here" },
+      { title: "Finance Fundamentals", link: "redux-framework-link-here" },
+      { title: "Options Trading", link: "web-development-link-here" },
+      { title: "Financial Modeling", link: "reactjs-link-here" },
+      { title: "Word", link: "css-link-here" },
+      { title: "Presenting", link: "redux-framework-link-here" },
+    ],
+    [
+      { title: "Technical Analysis", link: "typescript-link-here" },
+      { title: "Web Development", link: "javascript-link-here" },
+      { title: "IOT", link: "angular-link-here" },
+      { title: "Robotics", link: "nodejs-link-here" },
+      { title: "Database Management", link: "typescript-link-here" },
+      { title: "Mobile Development", link: "javascript-link-here" },
+      { title: "Statistics", link: "angular-link-here" },
+      { title: "IMP", link: "nodejs-link-here" },
     ],
   ];
 
@@ -87,39 +108,12 @@ const ButtonCarousel = () => {
           >
             <div className="d-flex flex-wrap">
               {page.map((item, itemIndex) => (
-                <div key={itemIndex} className="col-3">
-                  <a
-                    href={item.link}
-                    className="btn btn-outline-dark text-center fs-4 my-2"
-                    style={{
-                      fontWeight: "900",
-                      width: "250px",
-                      marginLeft: "10px",
-                      paddingRight: "10px",
-                      marginTop: "10px",
-                      marginBottom: "10px",
-                      height: "50px",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      borderRadius: "0",
-                      transition: "background-color 0.3s",
-                      backgroundColor: "transparent",
-                      color: "inherit",
-                      borderWidth: "0.0005px",
-                      borderColor: "#ADD8E6",
-                    }}
-                    onMouseEnter={(e) => {
-                      e.target.style.backgroundColor = "#F0FFFF";
-                      e.target.style.color = "inherit";
-                    }}
-                    onMouseLeave={(e) => {
-                      e.target.style.backgroundColor = "transparent";
-                      e.target.style.color = "inherit";
-                    }}
+                <div key={itemIndex} className="col-md-3">
+                  <CardContainer
+                    className="m-2"
                   >
-                    {item.title}
-                  </a>
+                    <a href={`/courses/${item.link}`}>{item.title}</a>
+                  </CardContainer>
                 </div>
               ))}
             </div>
--- src/components/courses-sub/music/Buttons.jsx
+++ src/components/courses-sub/music/Buttons.jsx
@@ -1,28 +1,49 @@
 import React, { useState } from "react";
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
+import CardContainer from "../../../contexts/CardContainer";
 
-const ButtonCarousel = ({ links }) => {
+const ButtonCarousel = () => {
   const items = [
     [
-      { title: "Instrument", link: "music/instruments" },
-      { title: "Vocal", link: "music/vocal" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
+      { title: "Web Development", link: "web-development-link-here" },
+      { title: "Software Development", link: "reactjs-link-here" },
+      { title: "React JS", link: "css-link-here" },
+      { title: "Redux Framework", link: "redux-framework-link-here" },
+      { title: "Data Scientist", link: "web-development-link-here" },
+      { title: "Artificial Intelligence", link: "reactjs-link-here" },
+      { title: "HTML & CSS", link: "css-link-here" },
+      { title: "Accounting", link: "redux-framework-link-here" },
     ],
     [
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
-      { title: "Sample", link: "link" },
+      { title: "Stock Trading", link: "typescript-link-here" },
+      { title: "Financial Analysis", link: "javascript-link-here" },
+      { title: "Cryptocurrency", link: "angular-link-here" },
+      { title: "Forex Trading", link: "nodejs-link-here" },
+      { title: "Financial Market", link: "typescript-link-here" },
+      { title: "Alogorithm Anaylisys", link: "javascript-link-here" },
+      { title: "Investing", link: "angular-link-here" },
+      { title: "Excel", link: "nodejs-link-here" },
+    ],
+    [
+      { title: "Day Trading", link: "web-development-link-here" },
+      { title: "Quick Books Online", link: "reactjs-link-here" },
+      { title: "Book Keeping", link: "css-link-here" },
+      { title: "Finance Fundamentals", link: "redux-framework-link-here" },
+      { title: "Options Trading", link: "web-development-link-here" },
+      { title: "Financial Modeling", link: "reactjs-link-here" },
+      { title: "Word", link: "css-link-here" },
+      { title: "Presenting", link: "redux-framework-link-here" },
+    ],
+    [
+      { title: "Technical Analysis", link: "typescript-link-here" },
+      { title: "Web Development", link: "javascript-link-here" },
+      { title: "IOT", link: "angular-link-here" },
+      { title: "Robotics", link: "nodejs-link-here" },
+      { title: "Database Management", link: "typescript-link-here" },
+      { title: "Mobile Development", link: "javascript-link-here" },
+      { title: "Statistics", link: "angular-link-here" },
+      { title: "IMP", link: "nodejs-link-here" },
     ],
   ];
 
@@ -35,7 +56,6 @@ const ButtonCarousel = ({ links }) => {
   const handlePrevPage = () => {
     setActivePage((prevPage) => (prevPage - 1 + items.length) % items.length);
   };
-
   const arrowStyles1 = {
     color: "white",
     marginLeft: "-120px",
@@ -88,43 +108,12 @@ const ButtonCarousel = ({ links }) => {
           >
             <div className="d-flex flex-wrap">
               {page.map((item, itemIndex) => (
-                <div key={itemIndex} className="col-3">
-                  {item.link ? (
-                    <a
-                      href={item.link}
-                      className="btn btn-outline-dark text-center fs-4 my-2"
-                      style={{
-                        fontWeight: "900",
-                        width: "250px",
-                        marginLeft: "10px",
-                        paddingRight: "10px",
-                        marginTop: "10px",
-                        marginBottom: "10px",
-                        height: "50px",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        borderRadius: "0",
-                        transition: "background-color 0.3s",
-                        backgroundColor: "transparent",
-                        color: "inherit",
-                        borderWidth: "0.0005px",
-                        borderColor: "#ADD8E6",
-                      }}
-                      onMouseEnter={(e) => {
-                        e.target.style.backgroundColor = "#F0FFFF";
-                        e.target.style.color = "inherit";
-                      }}
-                      onMouseLeave={(e) => {
-                        e.target.style.backgroundColor = "transparent";
-                        e.target.style.color = "inherit";
-                      }}
-                    >
-                      {item.title}
-                    </a>
-                  ) : (
-                    item.content
-                  )}
+                <div key={itemIndex} className="col-md-3">
+                  <CardContainer
+                    className="m-2"
+                  >
+                    <a href={`/courses/${item.link}`}>{item.title}</a>
+                  </CardContainer>
                 </div>
               ))}
             </div>
