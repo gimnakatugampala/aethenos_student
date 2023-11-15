f32e6261f7cad308a24127d48609de5a0e675a08
gimnakatugampala <gimnakatugampala1@gmail.com>
2023-11-15 14:33:04 +0530

    2023_11_15_home_new_update

--- src/components/homes/home/course-range.jsx
+++ src/components/homes/home/course-range.jsx
@@ -29,6 +29,9 @@ const CourseRange = () => {
                     <CardContainer>
                         <h4>Expand your career opportunities with Python</h4>
                         <p>Take one of Udemy’s range of Python courses and learn how to code using this incredibly useful language. Its simple syntax and readability makes Python perfect for Flask, Django, data science, and machine learning. You’ll learn how to build everything from games to sites to apps. Choose from a range of courses that will appeal to both beginners and advanced developers alike.</p>
+                        <a href='/courses/it-software/hardware' className="edu-btn btn-small">
+                            Explore Python
+                        </a>
                         <CourseRangeList />
                     </CardContainer>
                     </Tab>
@@ -37,6 +40,9 @@ const CourseRange = () => {
                     <CardContainer>
                         <h4>Analyze and visualize data with Excel</h4>
                         <p>Take a Microsoft Excel course from Udemy, and learn how to use this industry-standard software. Real-world experts will show you the basics like how to organize data into sheets, rows and columns, and advanced techniques like creating complex dynamic formulas. Both small businesses and large companies use Excel to turn their raw data into actionable insights.</p>
+                        <a href='/courses/it-software/hardware' className="edu-btn btn-small">
+                            Explore Excel
+                        </a>
                         <CourseRangeList />
                     </CardContainer>
                     </Tab>
@@ -45,6 +51,9 @@ const CourseRange = () => {
                     <CardContainer>
                         <h4>Build websites and applications with Web Development</h4>
                         <p>The world of web development is as wide as the internet itself. Much of our social and vocational lives play out on the internet, which prompts new industries aimed at creating, managing, and debugging the websites and applications that we increasingly rely on.</p>
+                        <a href='/courses/it-software/hardware' className="edu-btn btn-small">
+                            Explore with Web Development
+                        </a>
                         <CourseRangeList />
                     </CardContainer>
                     </Tab>
@@ -53,6 +62,9 @@ const CourseRange = () => {
                     <CardContainer>
                         <h4>Grow your software development skills with JavaScript</h4>
                         <p>JavaScript is a text-based computer programming language used to make dynamic web pages. A must-learn for aspiring web developers or programmers, JavaScript can be used for features like image carousels, displaying countdowns and timers, and playing media on a webpage. With JavaScript online classes, you can learn to build interactive web applications, choose the best framework, and work with other programming languages like HTML and CSS.</p>
+                        <a href='/courses/it-software/hardware' className="edu-btn btn-small">
+                            Explore with JavaScript
+                        </a>
                         <CourseRangeList />
                     </CardContainer>
                     </Tab>
@@ -61,6 +73,9 @@ const CourseRange = () => {
                     <CardContainer>
                         <h4>Lead data-driven decisions with Data Science</h4>
                         <p>Data science application is an in-demand skill in many industries worldwide — including finance, transportation, education, manufacturing, human resources, and banking. Explore data science courses with Python, statistics, machine learning, and more to grow your knowledge. Get data science training if you’re into research, statistics, and analytics.</p>
+                        <a href='/courses/it-software/hardware' className="edu-btn btn-small">
+                            Explore with Data Science
+                        </a>
                         <CourseRangeList />
                     </CardContainer>
                     </Tab>
@@ -69,6 +84,9 @@ const CourseRange = () => {
                     <CardContainer>
                         <h4>Become an expert in cloud computing with AWS Certification</h4>
                         <p>Amazon Web Services (AWS) is a cloud computing platform with more than 200 featured services. Whether or not you aim for certification, an AWS course offers the theory and practical skills you need to land a job in cloud development, sales, engineering, networking, and more. The better you become at cloud computing, the more you can earn. Anyone can learn AWS skills, and with AWS online training, you can move at your own pace.</p>
+                        <a href='/courses/it-software/hardware' className="edu-btn btn-small">
+                            Explore with Amazon AWS
+                        </a>
                         <CourseRangeList />
                     </CardContainer>
                     </Tab>
