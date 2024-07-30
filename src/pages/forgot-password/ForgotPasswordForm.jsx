23648ac60fbc52288be2a39726026f316f9aa40c
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-07-30 16:27:25 +0530

    2024_7_30_fotgot_password_add_checklist

--- src/pages/forgot-password/ForgotPasswordForm.jsx
+++ src/pages/forgot-password/ForgotPasswordForm.jsx
@@ -3,11 +3,15 @@ import ErrorAlert from '../../functions/Alert/ErrorAlert';
 import { ChangeToNewPassword, SendEmailVerficationCode, VerifyCode } from '../../api';
 import ButtonLoadingMedium from '../../functions/Loading/ButtonLoadingMedium';
 import VerificationInput from 'react-verification-input';
+import dynamic from 'next/dynamic';
+
+const PasswordChecklist = dynamic(() => import('react-password-checklist'), { ssr: false });
 
 const ForgotPasswordForm = () => {
   const [email, setEmail] = useState('');
   const [btnLoading, setBtnLoading] = useState(false);
   const [showVerificationInputs, setShowVerificationInputs] = useState(false);
+  const [isPasswordStrength, setisPasswordStrength] = useState(false)
 
   const [codeSuccess, setcodeSuccess] = useState(false)
 
@@ -64,6 +68,11 @@ const ForgotPasswordForm = () => {
       return
     }
 
+    if(isPasswordStrength == false){
+      ErrorAlert("Error","Your Password should match the checklist")
+      return
+    }
+
     ChangeToNewPassword(VerficationCode,email,conPassword,setBtnLoading)
 
   }
@@ -98,6 +107,17 @@ const ForgotPasswordForm = () => {
                   />
                   {showPass ? (<span onClick={() => setshowPass(!showPass)} className="password-show"><i class="fas fa-eye-slash fa-lg"></i></span>) : (<span onClick={() => setshowPass(!showPass)} className="password-show"><i class="far fa-eye fa-lg"></i></span>)} 
                 </div>
+
+                <PasswordChecklist
+                rules={["minLength","specialChar","number","capital","match"]}
+                minLength={5}
+                value={password}
+                valueAgain={conPassword}
+                onChange={(isValid) => {
+                  console.log(isValid)
+                  setisPasswordStrength(isValid)
+                }}
+              />
 
                 <div className="form-group">
                   {btnLoading ? (
