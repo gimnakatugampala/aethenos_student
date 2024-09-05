8bf3f60569d13151b96dc5a98fe64434e74f7564
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-09-05 22:57:19 +0530

    2024_excel_plugin

--- src/pages/excel-plugin/[id].jsx
+++ src/pages/excel-plugin/[id].jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 import { LoginWithTokenForItemCode } from '../../api';
+import { CircularProgress, Container, Typography, Box } from '@mui/material';
+
 
 const Index = () => {
 
@@ -20,7 +22,7 @@ const Index = () => {
             window.location.href = redirectUrl
   
             // Perform the redirection
-            router.replace(redirectUrl);
+            // router.replace(redirectUrl);
           } else {
             // Handle failed authentication (e.g., show an error or redirect to login page)
             router.replace('/login');  // Redirect to a login page or show an error
@@ -32,10 +34,21 @@ const Index = () => {
     }, [loginToken, id, sectionID, curriclumID, SyllabusType]);
 
   return (
-    <div>
-    {/* Loading spinner or any indication that the authentication is in progress */}
-    <p>Authenticating...</p>
-  </div>
+    <Container
+    component="main"
+    maxWidth="xs"
+    sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 8 }}
+  >
+    <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
+      <CircularProgress />
+    </Box>
+    <Typography variant="h5" component="h2" gutterBottom>
+      Authenticating Excel Users
+    </Typography>
+    <Typography variant="body1">
+      Please wait while we authenticate your credentials and redirect you to your course.
+    </Typography>
+  </Container>
   )
 }
 
