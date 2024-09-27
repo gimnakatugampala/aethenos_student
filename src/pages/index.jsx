17391fac36991bd3fd431d805ee3fc6610963e48
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-09-27 21:57:28 +0530

    homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,7 +33,7 @@ export default function Home() {
 
     return (
         <Wrapper>
-            <SEO pageTitle={'Home Main'} />
+            <SEO pageTitle={'Home'} />
             <HomeMain />
         </Wrapper>
     )
