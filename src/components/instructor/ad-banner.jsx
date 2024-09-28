7b1bc51eed81c8aeab3525cc71dcadc8136a8175
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-09-28 11:27:08 +0530

    grammar

--- src/components/instructor/ad-banner.jsx
+++ src/components/instructor/ad-banner.jsx
@@ -273,8 +273,8 @@ const AdBanner = () => {
                   id="terms-condition"
                 />
                 <label htmlFor="terms-condition">
-                  I agree the User Agreement and{" "}
-                  <Link href="/terms-condition">Terms & Condition.</Link>
+                  I agree to <Link href="/user-agreement">User Agreement</Link> and{" "}
+                  <Link href="/terms-condition">Terms & Conditions.</Link>
                 </label>
               </div>
             </div>
