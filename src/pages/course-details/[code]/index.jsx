2272cd5373c76223370143d747e8bb3ca7212a54
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-10-30 17:36:24 +0530

    course_details

--- src/pages/course-details/[code]/index.jsx
+++ src/pages/course-details/[code]/index.jsx
@@ -1,50 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import SEO from '../../../components/seo';
 import { Wrapper } from '../../../layout';
 import CourseDetailsMain from '../../../components/course-details';
-import CourseDetailsTwoMain from '../../../components/course-details-2'
-import { course_data } from '../../../data';
-import { useEffect } from 'react';
-import { GetCourseDetails, ValidateCouponOnCartFromCourseDetails } from '../../../api';
-import { useState } from 'react';
-
 import LargeLoading from '../../../functions/Loading/LargeLoading';
-
-// const course = course_data[0];
+import { GetCourseDetails, IMG_HOST, ValidateCouponOnCartFromCourseDetails } from '../../../api';
+import FormatHTMLRemove from '../../../functions/FormatHTMLRemove';
 
 const CourseDetails = () => {
-
   const router = useRouter();
-  const { code, couponCode  } = router.query;
-  const [course, setcourse] = useState(null)
+  const { code, couponCode } = router.query;
+  const [course, setCourse] = useState(null);
 
   useEffect(() => {
+    if (!router.isReady || !code) return; // Ensure the router is ready and code is available
 
-    if(code != null){
-      GetCourseDetails(code,setcourse)
+    // Fetch course details
+    GetCourseDetails(code, setCourse);
+
+    // Validate coupon if available
+    if (couponCode) {
+      ValidateCouponOnCartFromCourseDetails(couponCode, router, code);
     }
+  }, [router.isReady, code, couponCode]);
 
-    if(couponCode != null){
-      console.log(couponCode)
-      ValidateCouponOnCartFromCourseDetails(couponCode, router, code)
-    }
-   
-  }, [code])
-  
-
-    return (
-        <Wrapper>
-           {course != null ? (
-            <SEO pageTitle={`${course.title}`} />
-           ) : <SEO pageTitle={`Loading...`} />
-           }
-
-            {course != null ? (
-            <CourseDetailsMain course={course} />
-            ) : <LargeLoading />}
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <SEO imageUrl={course ? `${IMG_HOST}${course.img}` : 'Loading...'} description={course ? `${FormatHTMLRemove(course.course_main_desc)}` : 'Loading...'} pageTitle={course ? course.title : 'Loading...'} />
+      {course ? (
+        <CourseDetailsMain course={course} />
+      ) : (
+        <LargeLoading />
+      )}
+    </Wrapper>
+  );
+};
 
 export default CourseDetails;
