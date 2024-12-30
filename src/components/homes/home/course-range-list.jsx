5e98faf24d6a363efb350b287fc9584b8f892acb
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-12-30 22:44:47 +0530

    homepage

--- src/components/homes/home/course-range-list.jsx
+++ src/components/homes/home/course-range-list.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 const CourseRangeList = ({ course }) => {
   // console.log(course)
 
-  const coursePerView = 20;
+  const coursePerView = 8;
   const [next, setNext] = useState(coursePerView);
   const [courses, setCourses] = useState(course);
 
@@ -47,9 +47,9 @@ const CourseRangeList = ({ course }) => {
           onClick={handleLoadData}
           className="load-more-btn"
           data-sal-delay="100"
-          data-sal="slide-up"
+          // data-sal="slide-up"
           data-sal-duration="1200"
-          style={{ display: next < courses ? "block" : "none" }}
+          // style={{ display: next < courses ? "block" : "none" }}
         >
           <a className="edu-btn mb-5" style={{ cursor: "pointer" }}>
             Load More <i className="icon-56"></i>
