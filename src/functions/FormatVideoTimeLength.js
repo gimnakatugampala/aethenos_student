33801dbdb36117b3ea2cce028d8cbe85b1fabe50
gimnakatugampala <gimnakatugampala1@gmail.com>
2025-01-24 14:13:01 +0530

    2024_video_length

--- src/functions/FormatVideoTimeLength.js
+++ src/functions/FormatVideoTimeLength.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 export const FormatVideoTimeLength = (seconds) => {
     const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
+    let minutes = Math.floor((seconds % 3600) / 60);  // Use 'let' to allow reassignment
     const leftoverSeconds = seconds % 60;
 
     let timeString = "";
+
+    // If only seconds are present and less than 60 seconds, round up to 1 minute
+    if (hours === 0 && minutes === 0 && leftoverSeconds > 0) {
+        minutes = 1;
+    }
 
     // Add hours, minutes, and seconds with short labels if they are greater than 0
     if (hours > 0) {
